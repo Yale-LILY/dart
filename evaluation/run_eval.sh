c165bc637382013a44767eb229ed7a35e94804eb
Rui Zhang <r.zhang@yale.edu>
2020-07-02 15:46:06 -0400

    update

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -3,33 +3,45 @@
 # This serves our evaluation scripts and need to add to github
 
 # Let's use the following directory for evaluation of BLEU/METEOR/TER/MoverScore/BERTScore/BLEURT
-EVAL_DIR=/home/lily/ch956/eval
-cd ${EVAL_DIR}
+# EVAL_DIR=/home/lily/ch956/eval
+# cd ${EVAL_DIR}
 
 
 #################### Table 4 ##########################
-# Assume /home/lily/xarutang/yale/dtt/results has correct input, delexicalization, and in the correct order
-OUT_DIR=/home/lily/xarutang/yale/dtt/results
+TEST_TARGETS_REF0=/home/lily/ch956/eval/test-webnlg-all-notdelex.lex
 
-TEST_TARGETS_REF0=test-webnlg-all-notdelex.lex
+# Assume /home/lily/xarutang/yale/dtt/results has correct input, delexicalization, and in the correct order
 
 ### train on WebNLG only ###
+OUTPUT_FILE=/home/lily/xarutang/yale/dtt/results/relexicalised_webnlg_only.txt
 # BLEU all
-./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUT_DIR}/relexicalised_webnlg_only.txt
+./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUTPUT_FILE}
 # BLEU seen
 # BLEU unseen
 
+## TODO: need to make metrics.py have arguments
+python metrics.py
+
 # METEOR all
+cd ../meteor-1.5/ 
+java -Xmx2G -jar meteor-1.5.jar ../eval/relexicalised_predictions.txt ../eval/all-notdelex-ref-meteor.txt -l en -norm -r 8 > ../eval/meteor.txt
+cd ../eval ; tail -10 meteor.txt
 # METEOR seen
 # METEOR unseen
 
 # TER all
+cd ../tercom-0.7.25/
+java -jar tercom.7.25.jar -h ../eval/relexicalised_predictions-ter.txt -r ../eval/all-notdelex-refs-ter.txt > ../eval/ter.txt
+cd ../eval ; tail -10 ter.txt
 # TER seen
 # TER unseen
 
+
+
 ### ablation exp1 ###
+OUTPUT_FILE=/home/lily/xarutang/yale/dtt/results/relexicalised_exp1.txt
 # BLEU all
-./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUT_DIR}/relexicalised_exp1.txt
+./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUTPUT_FILE}
 # BLEU seen
 # BLEU unseen
 
@@ -42,8 +54,9 @@ TEST_TARGETS_REF0=test-webnlg-all-notdelex.lex
 # TER unseen
 
 ### ablation exp2 ###
+OUTPUT_FILE=/home/lily/xarutang/yale/dtt/results/relexicalised_exp2.txt
 # BLEU all
-./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUT_DIR}/relexicalised_exp2.txt
+./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUTPUT_FILE}
 # BLEU seen
 # BLEU unseen
 
@@ -57,8 +70,9 @@ TEST_TARGETS_REF0=test-webnlg-all-notdelex.lex
 
 
 ### ablation exp3 ###
+OUTPUT_FILE=/home/lily/xarutang/yale/dtt/results/relexicalised_exp3.txt
 # BLEU all
-# ./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUT_DIR}/relexicalised_exp3.txt
+# ./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUTPUT_FILE}
 # BLEU seen
 # BLEU unseen
 
@@ -73,13 +87,34 @@ TEST_TARGETS_REF0=test-webnlg-all-notdelex.lex
 
 #################### Table 5 ##########################
 
+TEST_TARGETS_REF0=/data/lily/ass52/webnlg/dart-v1.0.0/test-webnlg-all-notdelex.lex
+
+
 ### Seq-to-Seq with Attention ###
+OUTPUT_FILE=/data/lily/ass52/webnlg/dart-v1.0.0/relexicalised_predictions.txt
 # BLEU
+./multi-bleu.perl ${TEST_TARGETS_REF0} < ${OUTPUT_FILE}
+## TODO: need to make metrics.py have arguments
+python metrics.py
 # METEOR
+cd ../meteor-1.5/ 
+java -Xmx2G -jar meteor-1.5.jar ../eval/relexicalised_predictions.txt ../eval/all-notdelex-ref-meteor.txt -l en -norm -r 8 > ../eval/meteor.txt
+cd ../eval ; tail -10 meteor.txt
 # TER
+cd ../tercom-0.7.25/
+java -jar tercom.7.25.jar -h ../eval/relexicalised_predictions-ter.txt -r ../eval/all-notdelex-refs-ter.txt > ../eval/ter.txt
+cd ../eval ; tail -10 ter.txt
+
+
+source /home/lily/ch956/expenv/bin/activate
 # MoverScore
+python moverscore.py ${TEST_TARGETS_REF0} ${OUTPUT_FILE}
 # BERTScore
+bert-score -r ${TEST_TARGETS_REF0} -c ${OUTPUT_FILE} --lang en
 # BLEURT
+python -m bleurt.score -candidate_file=${OUTPUT_FILE} -reference_file=${TEST_TARGETS_REF0} -bleurt_checkpoint=../bleurt/bleurt/test_checkpoint -scores_file=scores
+python get_avg.py scores
+
 # GRandPARENT
 
 ### End-to-End Transformer ###
@@ -99,8 +134,6 @@ TEST_TARGETS_REF0=test-webnlg-all-notdelex.lex
 # BERTScore
 # BLEURT
 # GRandPARENT
-
-
 
 
 
